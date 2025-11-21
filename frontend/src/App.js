dd174c3520555aea8a9ab504fd51548d316beb11
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-11-21 14:42:59 -0600

    Inital Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'; //Header does not need to be in {} because it was export default from the file
+
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
