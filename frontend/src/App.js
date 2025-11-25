67160972b6af527fa5f1901b0fe3add0aafea47f
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-11-24 20:58:00 -0600

    Added Search component and update Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; //Header does not need to be in {} because it was export default from the file
+import Search from './components/Search';
+
 
 function App() {
   return (
     <div className="App">
       <Header title="Images Gallery"/>
+      <Search />
     </div>
   );
 }
