a3f7da8cc789d5fdb86283993924c7925ef2e9ef
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-11-25 16:57:17 -0600

    Add Search form and integrate into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
+import { use, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; //Header does not need to be in {} because it was export default from the file
 import Search from './components/Search';
 
-
-function App() {
+const App= () => {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log(word);
+  }
+  console.log(word);
   return (
     <div className="App">
       <Header title="Images Gallery"/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSearchSubmit={handleSearchSubmit} />
     </div>
   );
 }
