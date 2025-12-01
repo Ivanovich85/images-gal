faec7c76c935f57cbcacfff7b29256a67827a26c
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-12-01 11:30:32 -0600

    Controlled Search component

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -12,7 +12,7 @@ const Search = ({word, setWord, handleSubmit}) => {
                 <Form.Control 
                     type="text"
                     value={word}
-                    onChange={(e) => setWord(e.target.value)}
+                    onChange={(e) => setWord(e.target.value)}//this is what changes the state in App.js
                     placeholder="Search for new image..." 
                 />
               </Col>
