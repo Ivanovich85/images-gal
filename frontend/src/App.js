45ff70d291a5c71c82a54225fbc2b41365daeffa
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-12-08 14:34:09 -0600

    First api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; //Header does not need to be in {} because it was export default from the file
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;// this is how we access env variables in react in a folder ignored by git or a locked file 
+
 const App= () => {
   const [word, setWord] = useState('')
+
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/random/photos/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  console.log(word);
+
   return (
     <div className="App">
       <Header title="Images Gallery"/>
