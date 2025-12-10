b01a9c30ab19b7878648b0a5392b8a4fb3a9d097
Darwin CFO <Darwin.Ivanovich@gmail.com>
2025-12-10 14:13:59 -0600

    fixing code using linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
-import { use, useState } from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; //Header does not need to be in {} because it was export default from the file
 import Search from './components/Search';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;// this is how we access env variables in react in a folder ignored by git or a locked file 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY; // this is how we access env variables in react in a folder ignored by git or a locked file
 
-const App= () => {
-  const [word, setWord] = useState('')
+const App = () => {
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -19,17 +21,17 @@ const App= () => {
       .catch((error) => {
         console.log(error);
       });
-    setWord('');//clears the input field after submission
-  }
+    setWord(''); //clears the input field after submission
+  };
 
-    console.log(word);
+  console.log(word);
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
+      <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const Search = ({word, setWord, handleSubmit}) => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
@@ -9,11 +9,11 @@ const Search = ({word, setWord, handleSubmit}) => {
           <Form onSubmit={handleSubmit}>
             <Form.Row>
               <Col xs={9}>
-                <Form.Control 
-                    type="text"
-                    value={word}
-                    onChange={(e) => setWord(e.target.value)}//this is what changes the state in App.js
-                    placeholder="Search for new image..." 
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)} //this is what changes the state in App.js
+                  placeholder="Search for new image..."
                 />
               </Col>
               <Col>
